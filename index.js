a480f54d437ff2de4cee447699b665a474a2cfa6
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2022-12-06 17:24:34 +0100

    added post and get method

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+
+const User = require("./models/User");
 
 const app = express();
 dotenv.config();
@@ -10,10 +13,37 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.status(200).json("Home URL");
+app.get("/", async (req, res) => {
+  try {
+    const users = await User.find();
+    console.log("USERS FETCHED");
+    res.status(200).json(users);
+  } catch (error) {
+    console.log(error);
+  }
+});
+
+app.post("/", async (req, res) => {
+  try {
+    const user = new User({
+      username: req.body.username,
+      password: req.body.password,
+      email: req.body.email,
+      reports: [],
+    });
+
+    await user.save();
+    console.log("USERS ADDED");
+    res.status(200).json(user);
+  } catch (error) {
+    console.log(error);
+  }
 });
 
 app.listen(PORT, () => {
-  console.log(`server started on port ${PORT}`);
+  console.log(`backend server started on port ${PORT}`);
+  mongoose
+    .connect(process.env.MONGODB)
+    .then((result) => console.log("mongodb connected"))
+    .catch((error) => console.log(error));
 });
