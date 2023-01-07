95531ffb233736878ae40505f97cf7c8046febb7
Gabriel Dela Pena <delapena.gabriel12@gmail.com>
2023-01-07 20:27:30 +0100

    Testing upload image

--- index.js
+++ index.js
@@ -2,9 +2,11 @@ const express = require("express");
 const mongoose = require("mongoose");
 const cors = require("cors");
 const dotenv = require("dotenv");
+const multer = require("multer");
 
 const userRoutes = require("./routes/user");
 const reportRoutes = require("./routes/report");
+const Image = require("./models/Image");
 
 const app = express();
 dotenv.config();
@@ -14,13 +16,47 @@ const PORT = process.env.PORT || 3030;
 app.use(express.json());
 app.use(cors());
 
+// Storage
+const Storage = multer.diskStorage({
+  destination: "uploads",
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({
+  storage: Storage,
+}).single("image");
+
 app.get("/", async (req, res) => {
   res.status(200).json("Welcome to my API");
 });
 
+// Middleware upload image
+app.post("/upload", (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      const newImage = new Image({
+        image: {
+          data: req.file.filename,
+          contentType: "image/jpeg",
+        },
+      });
+      try {
+        newImage.save();
+        res.status(200).send("Upload success.");
+      } catch (error) {
+        console.log(error);
+        res.status(500).send("Error in da Server");
+      }
+    }
+  });
+});
+
 app.use("/api/users", userRoutes);
 app.use("/api/reports", reportRoutes);
-
 
 app.listen(PORT, () => {
   console.log(`backend server started on port ${PORT}`);
